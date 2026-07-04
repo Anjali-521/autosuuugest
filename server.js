2fed875fe733102a8b9981c0575119399fb680d3
Anjali <25wh5a0521@bvrithyderabad.edu.in>
2026-07-04 10:00:37 +0530

    Random user

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3002
+//console.log("frontend");
 app.use(express.static("frontend"))
 app.listen(port, function(){
     console.log("succeefully running at http://localhost:"+port)
